70b23c1c58daff232335b70a012a32e46f9d4906
Joaquín Duro Duro <joaquin.duro@gmail.com>
2017-11-06 20:22:37 +0100

    - Inicialización del proyecto.

---
+++ Lab1/main.c
@@ -0,0 +1,54 @@
+#include <OpenGL/OpenGL.h>
+#include <GLUT/GLUT.h>
+#include <time.h>
+#include "solution.h"
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define MARGIN_WIDTH 40
+
+GLint WindowWidth = WINDOW_WIDTH;
+GLint WindowHeight = WINDOW_HEIGHT;
+
+void init_gl(int argc, char * argv[]) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGB);
+  glutInitWindowPosition(50, 50);
+  glutInitWindowSize(WindowWidth, WindowHeight);
+  glutCreateWindow(argv[0]);
+  glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+void reshape_func(GLint width, GLint height) {
+  glViewport(0, 0, WindowWidth=width, WindowHeight=height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(-WindowWidth/2, WindowWidth/2, -WindowHeight/2, WindowHeight/2);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+void display_func() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
+  // Compute data
+  GLuint radius = (WindowWidth<WindowHeight)?WindowWidth:WindowHeight;
+  radius = (radius/2) - MARGIN_WIDTH;
+  time_t rawtime;
+  struct tm *timeinfo;
+  time ( &rawtime );
+  timeinfo = localtime (&rawtime);
+  // Draw the clock
+  draw_hoop(0, 0, radius);
+  draw_marks(0, 0, radius);
+  draw_hands(0, 0, radius, timeinfo->tm_hour, timeinfo->tm_min);
+  glFlush();
+}
+
+int main(int argc, char * argv[]) {
+  init_gl(argc, argv);
+  glutReshapeFunc(reshape_func);
+  glutDisplayFunc(display_func);
+  glutMainLoop();
+  return 0;
+}
---
+++ Lab1/solution.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include "solution.h"
+
+//Dibuja la forma del reloj, pintando la circunferencia exterior
+void draw_hoop(GLuint center_x, GLuint center_y, GLuint radius) {
+    
+}
+
+//Dibuja las marcas horarias del reloj
+//TODO: draw_marks no considera el centro.  Al cambiar el centro fallará.
+void draw_marks(GLuint center_x, GLuint center_y, GLuint radius) {
+    
+}
+
+/*Dibuja las manillas de horas y minutos en función de la hora actual pasada por los parámetros hour y minute*/
+void draw_hands(GLuint center_x, GLuint center_y, GLuint radius, GLuint hour, GLuint minute) {
+    
+}
